05b41905ab1f39f6c882f064a356b45417463838
Emmanuel Cobian Duarte <ecobian12310@gmail.com>
2023-10-28 10:13:49 -0700

    added new file upload system

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useMutation } from "convex/react";
 import { UploadDropzone } from "@xixixao/uploadstuff/react";
 import "@xixixao/uploadstuff/react/styles.css";
@@ -9,18 +10,37 @@ function App() {
   const saveAfterUpload = async (uploaded) => {
     await saveStorageId({ storageId: (uploaded[0].response).storageId });
   };
+
+  const [selectedImage, setSelectedImage] = useState(null);
  
   return (
     <>
-      <UploadDropzone
+      {/* <UploadDropzone
         uploadUrl={generateUploadUrl}
-        fileTypes={[".pdf", "image/*"]}
+        fileTypes={["image/*"]}
         onClientUploadComplete={saveAfterUpload}
         onUploadError={(error) => {
           // Do something with the error.
           alert(`ERROR! ${error}`);
         }}
-      />
+      /> */}
+      {selectedImage && (
+        <div>
+          <img 
+            alt='not found'
+            width={250}
+            src={URL.createObjectURL(selectedImage)}
+          />
+          <br />
+          <button onClick={() => setSelectedImage(null)}>Remove</button>
+        </div>
+      )}
+      <br /><br />
+
+      <input type='file' name='image' onChange={(e) => {
+        console.log(e.target.files[0])
+        setSelectedImage(e.target.files[0])
+      }}/>
     </>
   );
 }
