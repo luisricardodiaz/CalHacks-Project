ddcf09c0bbfaa468f9245ce378d1e2cbad6c11bf
Emmanuel Cobian Duarte <ecobian12310@gmail.com>
2023-10-27 21:42:40 -0700

    added night time songs

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,8 +14,9 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as _schema from "../_schema";
 import type * as myFunctions from "../myFunctions";
+import type * as tasts from "../tasts";
+import type * as _schema from "../_schema";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -26,8 +27,9 @@ import type * as myFunctions from "../myFunctions";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  _schema: typeof _schema;
   myFunctions: typeof myFunctions;
+  tasts: typeof tasts;
+  _schema: typeof _schema;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
