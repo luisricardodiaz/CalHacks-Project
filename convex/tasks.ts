d3d032cdea67e270a97a8857c9ca8e0b0d09861e
Ricky Diaz <81951376+luisricardodiaz@users.noreply.github.com>
2023-10-28 17:54:16 -0700

    Emans branch (#3)
    * added new file upload system
    
    * added spotify log in functionality
    
    * a
    
    ---------
    
    Co-authored-by: Emmanuel Cobian Duarte <ecobian12310@gmail.com>

--- convex/tasks.ts
+++ convex/tasks.ts
@@ -2,9 +2,9 @@ import { mutation } from "./_generated/server";
 import { v } from "convex/values";
 
 export const createTask = mutation({
-  args: { text: v.string() },
+  args: { environment: v.string(), time_of_day: v.string(), added_at: v.string(), added_by: v.any(), is_local: v.boolean(), primary_color: v.any(), track: v.any(), video_thumbnail: v.any()},
   handler: async (ctx, args) => {
-    const taskId = await ctx.db.insert("tasks", { text: args.text });
+    const taskId = await ctx.db.insert("songs", { environment: args.environment, time_of_day: args.time_of_day, added_at: args.added_at, added_by: args.added_by, is_local: args.is_local, primary_color: args.primary_color, track: args.track, video_thumbnail: args.video_thumbnail });
     // do something with `taskId`
     return taskId;
   },
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useQuery } from "convex/react"
+import axios from "axios";
+import { useQuery, useMutation } from "convex/react"
 import { api } from "../convex/_generated/api";
 import { Button } from "react-bootstrap";
 import React, { useEffect, useRef, useState } from 'react';
@@ -8,11 +9,24 @@ import { Waveform } from '@uiball/loaders';
 const images = ['src/assets/image1.jpeg', 'src/assets/image2.jpeg', 'src/assets/image3.jpeg', 'src/assets/image4.jpeg', 'src/assets/image5.jpeg', 'src/assets/image6.jpeg', 'src/assets/image7.jpeg', 'src/assets/image8.jpeg', 'src/assets/image9.jpeg', 'src/assets/image10.jpeg', 'src/assets/image11.jpeg']
 
 function App() {
+  const CLIENT_ID = "ff3b13358d7a4b2bbc5a0f4df6112b5d";
+  const REDIRECT_URI = "http://localhost:5173/";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+  const [token, setToken] = useState("");
+  const mutatePlaylist = useMutation(api.tasks.createTask);
+  const playlistToVibe = {
+    "Forest": {"Sunrise" : "Liminal Space Mix", "DayTime": "Autumn Mix", "Nighttime": "Running Mix"}, 
+    "Buildings": {"Sunrise" : "Hype Morning Mix", "DayTime": "Driving Mix", "Nighttime": "Late Night Mix"},
+    "Mountains": {"Sunrise" : "Feel Good Morning Mix", "DayTime": "Happy Camping Mix", "Nighttime": "Camping Night Time Mix"},
+    "Sea": {"Sunrise" : "Surfing Mix", "DayTime": "Ocean Mix", "Nighttime": "Nature Mix"}
+  }
   const [isLoading, setIsLoading] = useState(false);
   const [imageUploaded, setImageUploaded] = useState(false)
   const [imageUrl, setImageUrl] = useState("")
   const [imageName, setImageName] = useState("")
   useEffect(() => {
+  
     async function asyncWrapper() {
       if (imageUploaded == false) {
         return
@@ -34,6 +48,61 @@ function App() {
         }
         return result;
       }
+      
+      const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
+  const renderPlaylistSongs = (tracks, environment, timeOfDay) => {
+    tracks.map((track) => {
+      mutatePlaylist({ environment: environment, time_of_day: timeOfDay, added_at: track.added_at, added_by: track.added_by, is_local: track.is_local, primary_color: track.primary_color, track: track.track, video_thumbnail: track.video_thumbnail })
+    });
+  }
+
+  const searchTracks = async (apiEndpoint, environment, timeOfDay) => {
+    const { data } = await axios.get(apiEndpoint, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    renderPlaylistSongs(data.tracks.items, environment, timeOfDay)
+  }
+
+  const searchPlaylist = async (e) => {
+    e.preventDefault();
+    for (const environment in playlistToVibe) {
+      for (const timeOfDay in playlistToVibe[environment]) {
+        console.log(playlistToVibe[environment][timeOfDay])
+        const { data } = await axios.get("https://api.spotify.com/v1/search", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          params: {
+            q: playlistToVibe[environment][timeOfDay],
+            type: "playlist",
+          },
+        });
+        searchTracks(data.playlists.items[0].href, environment, timeOfDay);
+      }
+    }
 
       async function timeQuery(filename) {
         const data = await (await fetch(filename)).arrayBuffer()
@@ -193,6 +262,19 @@ function App() {
             color= {isLightMode ? 'black' : 'white'}
           />
         </div>
+        {!token ? (
+        <a
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+        >
+          Login to Spotify
+        </a>
+      ) : (
+        <button onClick={logout}>Logout</button>
+      )}
+      <form onSubmit={searchPlaylist}>
+        {/* <input type="text" onChange={(e) => setSearchKey(e.target.value)} /> */}
+        <button type={"submit"}>Begin Table Population</button>
+      </form>
       </div>
     </>
   );
